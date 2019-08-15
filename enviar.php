bc18a829c8686dda7e9b947745e3bed0a903941f
SergioGodinez <54123881+SergioGodinez@users.noreply.github.com>
2019-08-14 22:38:22 -0500

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,9 +1,9 @@
 <?php
  $destino= "omar.gdz18@gmail.com"
  $nombre= $_POST["nombre"];
- $correo= $_POST["correo"]
- $telefono= $_POST["telefono"]
- $mensaje= $_POST["mensaje"]
+ $correo= $_POST["correo"];
+ $telefono= $_POST["telefono"];
+ $mensaje= $_POST["mensaje"];
  $contenido = "Nombre: ".$nombre . "\nCorreo: ".$correo. "\nTelefono : ".$telefono. "Mensaje: ".$mensaje;
 
  mail($destino,"Contacto",$contenido);
